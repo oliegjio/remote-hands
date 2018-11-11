74d1a07249c5ec23a4d5a32422be079b0c51fdfe
vadim8960 <akimovvadim779@gmail.com>
2018-11-11 13:57:52 +0300

    Connected my smartphone to ESP

--- ESP/scetch-1/scetch-1.ino
+++ ESP/scetch-1/scetch-1.ino
@@ -1,11 +1,31 @@
+#include <ESP8266WiFi.h>
 #include <MPU6050.h>
 
-const byte ledPin = 13;
+MPU6050 mpu;
 
+const char* ssid = "ssid name";
+const char* password = "ssid password";
+ 
+int ledPin = D5;
+WiFiServer server(80);
+ 
 void setup() {
-    
+  Serial.begin(115200);
+  delay(10);
+ 
+ 
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+ 
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
 }
 
 void loop() {
-    
+  Serial.println("fuck");
 }
