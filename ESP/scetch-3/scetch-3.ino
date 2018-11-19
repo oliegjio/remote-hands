f3e337a919966a5e09a036cf9ac5b34e72f6126e
vadim8960 <akimovvadim779@gmail.com>
2018-11-19 17:28:30 +0300

    Commit

--- ESP/scetch-3/scetch-3.ino
+++ ESP/scetch-3/scetch-3.ino
@@ -1,88 +1,102 @@
-/************************************************************
-MPU9250_DMP_Quaternion
- Quaternion example for MPU-9250 DMP Arduino Library 
-Jim Lindblom @ SparkFun Electronics
-original creation date: November 23, 2016
-https://github.com/sparkfun/SparkFun_MPU9250_DMP_Arduino_Library
-
-The MPU-9250's digital motion processor (DMP) can calculate
-four unit quaternions, which can be used to represent the
-rotation of an object.
-
-This exmaple demonstrates how to configure the DMP to 
-calculate quaternions, and prints them out to the serial
-monitor. It also calculates pitch, roll, and yaw from those
-values.
-
-Development environment specifics:
-Arduino IDE 1.6.12
-SparkFun 9DoF Razor IMU M0
-
-Supported Platforms:
-- ATSAMD21 (Arduino Zero, SparkFun SAMD21 Breakouts)
-*************************************************************/
+#include <SPI.h>
+#include <Wire.h>
+#include <WiFiClient.h>
+#include <ESP8266WiFi.h>
 #include "SparkFunMPU9250-DMP.h"
 
-#define SerialPort Serial
 
+WiFiClient client;
 MPU9250_DMP imu;
 
-void setup() 
-{
-  SerialPort.begin(115200);
+const char *ssid  = "mint-pc";
+const char *password = "fFK5ieoY";
 
-  // Call imu.begin() to verify communication and initialize
-  if (imu.begin() != INV_SUCCESS)
-  {
-    while (1)
-    {
-      SerialPort.println("Unable to communicate with MPU-9250");
-      SerialPort.println("Check connections, and try again.");
-      SerialPort.println();
-      delay(5000);
+#define port 7247
+#define ip   IPAddress(10,42,0,1)
+
+//Forward functions
+float clamp(float value, float min_value, float max_value);
+String getData();
+void connect(const char *ssid, const char *password);
+
+
+void setup() {
+    Wire.begin();
+    Serial.begin(115200);
+    
+    if (imu.begin() != INV_SUCCESS) {
+        while (1) {
+            Serial.println("Unable to communicate with MPU-9250");
+            Serial.println("Check connections, and try again.");
+            Serial.println();
+            delay(5000);
+        }
     }
-  }
-  
-  imu.dmpBegin(DMP_FEATURE_6X_LP_QUAT | // Enable 6-axis quat
-               DMP_FEATURE_GYRO_CAL, // Use gyro calibration
-              10); // Set DMP FIFO rate to 10 Hz
-  // DMP_FEATURE_LP_QUAT can also be used. It uses the 
-  // accelerometer in low-power mode to estimate quat's.
-  // DMP_FEATURE_LP_QUAT and 6X_LP_QUAT are mutually exclusive
+    imu.dmpBegin(DMP_FEATURE_6X_LP_QUAT | DMP_FEATURE_GYRO_CAL, 10);
 }
 
-void loop() 
-{
-  // Check for new data in the FIFO
-  if ( imu.fifoAvailable() )
-  {
-    // Use dmpUpdateFifo to update the ax, gx, mx, etc. values
-    if ( imu.dmpUpdateFifo() == INV_SUCCESS)
-    {
-      // computeEulerAngles can be used -- after updating the
-      // quaternion values -- to estimate roll, pitch, and yaw
-      imu.computeEulerAngles();
-      printIMUData();
+void loop() {
+    if ( imu.fifoAvailable() ) {
+        if ( imu.dmpUpdateFifo() == INV_SUCCESS) {
+              imu.updateAccel();
+              imu.computeEulerAngles();
+              Serial.println(getData());
+              //client.println(getData());
+         }
     }
-  }
 }
 
-void printIMUData(void)
-{  
-  // After calling dmpUpdateFifo() the ax, gx, mx, etc. values
-  // are all updated.
-  // Quaternion values are, by default, stored in Q30 long
-  // format. calcQuat turns them into a float between -1 and 1
-  float q0 = imu.calcQuat(imu.qw);
-  float q1 = imu.calcQuat(imu.qx);
-  float q2 = imu.calcQuat(imu.qy);
-  float q3 = imu.calcQuat(imu.qz);
+///////////////////////////// ///////////////////////////////////
+/////             Desctiption functions                    //////
+/////////////////////////////////////////////////////////////////
 
-  SerialPort.println("Q: " + String(q0, 4) + ", " +
-                    String(q1, 4) + ", " + String(q2, 4) + 
-                    ", " + String(q3, 4));
-  SerialPort.println("R/P/Y: " + String(imu.roll) + ", "
-            + String(imu.pitch) + ", " + String(imu.yaw));
-  SerialPort.println("Time: " + String(imu.time) + " ms");
-  SerialPort.println();
+float clamp(float value, float min_value, float max_value) {
+    value = ( value > max_value ) ? max_value : value;
+    value = ( value < min_value ) ? min_value : value;
+    return value;
+}
+
+String getData() {
+    float q0 = imu.calcQuat(imu.qw);
+    float q1 = imu.calcQuat(imu.qx);
+    float q2 = imu.calcQuat(imu.qy);
+    float q3 = imu.calcQuat(imu.qz);
+    
+    String data = String(q0) + ' ';
+    data += String(q1) + ' ';
+    data += String(q2) + ' ';
+    data += String(q3) + ' ';
+
+    float ax = clamp(imu.calcAccel(imu.ax), -1.0, 1.0);
+    float ay = clamp(imu.calcAccel(imu.ay), -1.0, 1.0);
+    float az = clamp(imu.calcAccel(imu.az), -1.0, 1.0);
+
+    data += String(ax) + ' ';
+    data += String(ay) + ' ';
+    data += String(az) + ' ';
+
+    return data;
+}
+
+void connect(const char *ssid, const char *password) {
+    WiFi.mode(WIFI_OFF);
+    WiFi.begin(ssid, password);
+
+    while(WiFi.status() != WL_CONNECTED){
+        Serial.print(".");
+        delay(250);
+    }
+    Serial.println();
+    Serial.println("Connected to the WiFi");
+    Serial.println(ssid);
+    Serial.println(WiFi.localIP());
+
+    while (!client.connect(ip, port)) {
+        Serial.println("connection failed");
+        delay(2000);
+    }
+
+    Serial.println();
+    Serial.println("Connected to the Server");
+    delay(3000);
 }
