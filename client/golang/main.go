61b76e667cef81d066e3b6e6fb1cca05413b577f
oliegjio <oliegjio@gmail.com>
2018-11-20 16:10:40 +0300

    Working on TCP server

--- client/golang/main.go
+++ client/golang/main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"fmt"
-	// "./gonum/quat"
-	"net"
-	"time"
 	"os"
 )
 
-func check(err error) {
+func checkError(err error) {
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -16,33 +13,35 @@ func check(err error) {
 }
 
 func main() {
-	service := ":8080"
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
-	check(err)
-	listener, err := net.ListenTCP("tcp", tcpAddr)
-	check(err)
+	startTCPServer()
 
-	buffer := [] byte {}
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			continue
-		}
-		conn.SetDeadline(time.Now())
-		for {
-			if _, err := conn.Read(buffer); err != nil {
-				conn.Close()
-				conn = nil
-				fmt.Println(err)
-				continue
-			} else {
-				conn.SetDeadline(time.Now().Add(10 * time.Millisecond))
-			}
-			if len(buffer) == 0 {
-				continue
-			}
-			fmt.Println(buffer)
-		}
-	}
+	//service := ":8080"
+	//tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	//checkError(err)
+	//listener, err := net.ListenTCP("tcp", tcpAddr)
+	//checkError(err)
+	//
+	//buffer := [] byte {}
+	//
+	//for {
+	//	conn, err := listener.Accept()
+	//	if err != nil {
+	//		continue
+	//	}
+	//	conn.SetDeadline(time.Now())
+	//	for {
+	//		if _, err := conn.Read(buffer); err != nil {
+	//			conn.Close()
+	//			conn = nil
+	//			fmt.Println(err)
+	//			continue
+	//		} else {
+	//			conn.SetDeadline(time.Now().Add(10 * time.Millisecond))
+	//		}
+	//		if len(buffer) == 0 {
+	//			continue
+	//		}
+	//		fmt.Println(buffer)
+	//	}
+	//}
 }
